39109645a86f336942a3ff161ff8bf8891fc29e4
adunhumphrey <humphreyadun@gmail.com>
2025-04-06 20:11:23 +0100

    s

--- steel.py
+++ steel.py
@@ -63,8 +63,6 @@ def to_excel(df):
         df.to_excel(writer, index=False)
     processed_data = output.getvalue()
     return processed_data
-    
-st.subheader(f"View and Filter {dataset_name}")
 
                
 # Load data preview (first 1000 rows only)
@@ -82,7 +80,7 @@ if df_preview is not None:
     #st.dataframe(df_preview.head(), hide_index=True)
 
     # Milestone Image 
-    st.write("### Key Milestone")
+    st.write("### Key Milestones for Steel sector")
     st.image(milestone_image1)
 
     # Load full data for filtering purposes (without limiting to preview rows)
