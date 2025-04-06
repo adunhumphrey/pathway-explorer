02ac974422a646d4f40ac35009edf1d201c3847e
adunhumphrey <humphreyadun@gmail.com>
2025-04-06 20:36:19 +0100

    Update FLAG.py

--- FLAG.py
+++ FLAG.py
@@ -83,7 +83,7 @@ if df_preview is not None:
     #st.dataframe(df_preview.head(), hide_index=True)
 
     # Milestone Image 
-    st.write("### Key Milestone")
+    st.write("### Key Milestones for Forestry, Land and Agriculture (FLAG) sector")
     st.image(milestone_image1)
 
     # Load full data for filtering purposes (without limiting to preview rows)
