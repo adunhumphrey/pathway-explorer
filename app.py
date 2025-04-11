c0f0c5370885ceb689ac142041ab1b6b6bdf4af2
adunhumphrey <humphreyadun@gmail.com>
2025-04-11 07:55:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -156,7 +156,18 @@ pages = {
 
 # ✅ Set page config
 st.set_page_config(page_title="Pathway Explorer", layout="wide")
-
+st.markdown(
+"""
+<style>
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+.styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+.viewerBadge_text__1JaDK {
+display: none;
+}
+</style>
+""",
+unsafe_allow_html=True
+)
 # ✅ Get the selected page from URL reference (if exists)
 query_params = st.query_params
 selected_page = query_params.get("selected_page", None)
