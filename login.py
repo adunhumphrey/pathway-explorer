179f908ba00d3cfab1ee11a65ca0c0dcf55e0a58
adunhumphrey <humphreyadun@gmail.com>
2025-05-06 11:01:49 +0100

    Update login.py

--- login.py
+++ login.py
@@ -1,14 +1,25 @@
 import streamlit as st
 import importlib
+import base64
+
+# Function to encode image to base64
+def get_base64_image(image_path):
+    with open(image_path, "rb") as image_file:
+        encoded = base64.b64encode(image_file.read()).decode()
+    return f"data:image/png;base64,{encoded}"  # Change format if needed (png, jpg, etc.)
 
 USER_CREDENTIALS = {
     "humphrey": "password123@",
-    "eoin": "E1on12@",
-    "sbti_user": "pathwayexplorer",
+    "eoin": "E1on12@"
 }
 
+logo_image = get_base64_image("SBT_Logo.png")
+
 def login():
-    st.title("Login to SBTi pathway explorer")
+    st.markdown(f"""
+        <div style='text-align: center;'><img src="{logo_image}" width='300'></div>
+    """, unsafe_allow_html=True)  # Add the logo at the top and center it
+    st.markdown("<h1 style='font-family: Seaford; text-align: center;'>Login to Access the Dashboard</h1>", unsafe_allow_html=True)
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
 
@@ -16,7 +27,7 @@ def login():
         if username in USER_CREDENTIALS and USER_CREDENTIALS[username] == password:
             st.session_state["authenticated"] = True
             st.success("Login successful!")
-            st.rerun()()
+            st.rerun()
         else:
             st.error("Invalid username or password!")
 
